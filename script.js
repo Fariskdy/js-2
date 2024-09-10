5ca517dee89d155b711e2db142fc51be313e4db7
Fariskdy <farisfarizz500@gmail.com>
2024-09-10 20:11:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkOddOrEven(number) {
+  if (number % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+
+// usages
+checkOddOrEven(5);
+checkOddOrEven(8);
